bb288c2722c833d0f92c6fa85d09311ce576c109
Rebecca Gebhard <r.gebhard92@gmail.com>
2014-05-27 21:34:57 -0700

    probably wanted to add everything back in

---
+++ c++/sorting/DynamicArray.h
@@ -0,0 +1,48 @@
+/******************************
+Rebecca Gebhard
+DynamicArray.h
+**************************/
+
+#ifndef __DYNAMICARRAY_H__
+#define __DYNAMICARRAY_H__
+
+#include <iostream>
+#include <string>
+#include "file.h"
+
+using namespace std;
+
+ const string ERROR = "error\n";
+
+
+class iterAlreadyExists; 
+
+class DynamicArray{
+
+ public:
+  DynamicArray(); //ctr
+  ~DynamicArray();  //dctr
+  DynamicArray(const DynamicArray &copyMe); //cctr
+  DynamicArray& operator = (const DynamicArray &copyMe);//opovd
+  Site& operator[](unsigned int loc); //allow index
+  
+  Site* first;
+  void Delete();//deletes it 
+  void SetSize(int size); //changes the size
+  void Empty(); //empty arr
+  void Copy(const DynamicArray &copyMe); //copy copyMe to arr
+  void Append(const Site &element); //add new elt
+  unsigned int getSize(); //return sizeUsed
+  
+ private:
+  Site* arr;
+  int sizeUsed; //size in use/filled
+  int sizeTotal; //size allocated
+  const static int initSize = 500; //inital allocated size
+  const static int enlargeAmt = 2;  //expand factor
+};
+
+
+
+
+#endif
